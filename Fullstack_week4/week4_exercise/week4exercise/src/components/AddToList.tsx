f335230ed888f73beee0ad9ffd117094cee935f2
Denis <cph-dp153@cphbusiness.dk>
2023-02-23 09:59:46 +0100

    nået til at få updated person

--- Fullstack_week4/week4_exercise/week4exercise/src/components/AddToList.tsx
+++ Fullstack_week4/week4_exercise/week4exercise/src/components/AddToList.tsx
@@ -21,6 +21,37 @@ const AddToList: React.FC<IProps> = ( {setPeople, people}) => {
         })
     }
 
+    const removeLastFromList = () => {
+        setPeople(people.slice(0,-1))
+    }
+
+    const sortByAge = () => {
+        setPeople([...people].sort((a,b) => a.age - b.age))
+         }
+    
+        
+    const updatePerson = (
+          ): void => {
+            setPeople(people => people.map((person) => {
+              if (person.name === input.name) {
+                return {                        // <-- return new object reference
+                  ...person,                  // <-- shallow copy previous state
+                  age: parseInt(input.age), // <-- set property value
+                  occupation: input.occupation
+                }
+              }
+              return person; // <-- else return previous state
+            }));
+            ;
+        setInput( {
+            name: "",
+            age: "",
+            occupation: ""
+        })
+    };
+    
+
+         
     const handleClick = () => {
         if(
             !input.name  || !input.age
@@ -70,6 +101,15 @@ const AddToList: React.FC<IProps> = ( {setPeople, people}) => {
             />
             <button className="AddToList-btn" onClick={handleClick}>
                 Add to List
+            </button>
+            <button className="AddToList-btn" onClick={removeLastFromList}>
+                Remove last entry from List
+            </button>
+            <button className="AddToList-btn" onClick={sortByAge}>
+                Sort by age
+            </button>
+            <button className="AddToList-btn" onClick={updatePerson}>
+                Update person
             </button>
         </div>
     )
